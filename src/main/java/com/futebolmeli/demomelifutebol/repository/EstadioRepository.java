638b565b8d8eb058c6639c6778d35af569d2510a
develohome <develohome@gmail.com>
2025-07-18 21:15:19 -0300

    criacao de metodo no arquivo EstadioRepository

--- src/main/java/com/futebolmeli/demomelifutebol/repository/EstadioRepository.java
+++ src/main/java/com/futebolmeli/demomelifutebol/repository/EstadioRepository.java
@@ -4,4 +4,5 @@ import com.futebolmeli.demomelifutebol.entity.Estadio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EstadioRepository extends JpaRepository<Estadio, Long> {
+    boolean existsByNome(String nome);
 }
