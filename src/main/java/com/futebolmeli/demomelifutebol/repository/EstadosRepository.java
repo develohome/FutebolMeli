ff85dac440c4aab00b44dd342cbbcb6a438d0263
develohome <develohome@gmail.com>
2025-07-18 21:13:28 -0300

    criacao de metodo no EstadosRepository

--- src/main/java/com/futebolmeli/demomelifutebol/repository/EstadosRepository.java
+++ src/main/java/com/futebolmeli/demomelifutebol/repository/EstadosRepository.java
@@ -4,7 +4,10 @@ import com.futebolmeli.demomelifutebol.entity.Estados;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface EstadosRepository extends JpaRepository<Estados, Long> {
-
+    List<Boolean> findBySiglaContains(String sigla);
 }
